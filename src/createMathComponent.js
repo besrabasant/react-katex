dfad45cb1339a479edf365ab9b771a9b2eb68469
Basant <besrabasant@gmail.com>
2017-06-24 00:25:04 +0530

    use 'prop-types' for React Proptypes

--- src/createMathComponent.js
+++ src/createMathComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import KaTeX from 'katex';
 
 const createMathComponent = (Component, { displayMode }) => {
@@ -50,9 +51,9 @@ const createMathComponent = (Component, { displayMode }) => {
   }
 
   MathComponent.propTypes = {
-    children: React.PropTypes.string,
-    math: React.PropTypes.string,
-    renderError: React.PropTypes.func
+    children: PropTypes.string,
+    math: PropTypes.string,
+    renderError: PropTypes.func
   };
 
   return MathComponent;
